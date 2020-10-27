4d69f3ce706d134d009f1ec2bffbb8fd4cf2e726
ErikGraciosa <erikgraciosa@gmail.com>
2020-10-27 13:11:23 -0700

    initial structure layout, imports connected

---
+++ src/ImageItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import images from './data.js'
+
+export default class ImageItem extends Component {
+    render() {
+        return (
+            <div>
+                images here
+            </div>
+        )
+    }
+}
