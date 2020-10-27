2c3a3122edf58092a909840773138227da302511
ErikGraciosa <erikgraciosa@gmail.com>
2020-10-27 13:01:54 -0700

    initial react app buildout

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Here are the boogers
+    </div>
+  );
+}
+
+export default App;
